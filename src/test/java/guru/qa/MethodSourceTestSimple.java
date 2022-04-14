3c03cef83cda8fcc9de495e786caaba238afbea2
Elena Bodrenok <lena.bodrenok@gmail.com>
2022-04-14 22:27:41 +0300

    done

--- src/test/java/guru/qa/MethodSourceTestSimple.java
+++ src/test/java/guru/qa/MethodSourceTestSimple.java
@@ -3,21 +3,20 @@ package guru.qa;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.List;
 import java.util.stream.Stream;
 
 public class MethodSourceTestSimple {
     static Stream<Arguments> methodSourceTestSimple() {
         return Stream.of(
-                Arguments.of("Age", List.of(5), "years"),
-                Arguments.of("Height", List.of(43), "inch")
+                Arguments.of(List.of(1.5), "Sydney", "Melbourne"),
+                Arguments.of(List.of(2), "Sydney", "Hobart")
         );
     }
 
     @MethodSource("methodSourceTestSimple")
     @ParameterizedTest(name = "methodSourceTestSimple")
-    void methodSourceTestSimple(String valueOne, List<Integer> valueTwo, String valueThree) {
-        System.out.println(valueOne + " " + valueTwo + " " + valueThree);
+    void methodSourceTestSimple(List<Float> valueOne, String valueTwo, String valueThree) {
+        System.out.println(valueOne + " hour(s) flight from " + valueTwo + " to " + valueThree);
     }
 }
